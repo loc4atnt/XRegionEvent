277d27e7aa41957991d2c364518848156d644a11
loc4atnt <dorakid153@gmail.com>
2020-09-10 14:46:23 +0700

    final code

--- src/loc4atnt/xlibs/external/XRegionEvent/VersionManager.java
+++ src/loc4atnt/xlibs/external/XRegionEvent/VersionManager.java
@@ -12,6 +12,7 @@ import com.sk89q.worldguard.protection.regions.RegionContainer;
 
 public class VersionManager {
 
+//	-------------- for version 1.14 => .... ------------
 	public RegionManager getWGRegionManager(World world) {
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		return container.get(BukkitAdapter.adapt(world));
@@ -21,4 +22,16 @@ public class VersionManager {
 		RegionManager rm = getWGRegionManager(loca.getWorld());
 		return rm.getApplicableRegions(BlockVector3.at(loca.getX(), loca.getY(), loca.getZ()));
 	}
+//	-------------- for version 1.14 => .... ------------
+
+//	-------------- for version 1.10 => 1.12 ------------
+//	public RegionManager getWGRegionManager(World world) {
+//		return WGREvent.wgPlugin.getRegionManager(world);
+//	}
+//
+//	public ApplicableRegionSet getApplicableRegionSet(Location loca) {
+//		RegionManager rm = getWGRegionManager(loca.getWorld());
+//		return rm.getApplicableRegions(loca);
+//	}
+//	-------------- for version 1.10 => 1.12 ------------
 }
